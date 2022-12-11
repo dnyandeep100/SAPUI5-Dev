0b26a0d1af06b8c43aa822a53ec2d95d60ac906b
dnyandeep123 <dnyandeep7777@gmail.com>
2022-12-11 08:00:26 +0000

    First commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["devdemo/devdemo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"devdemo/devdemo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
